a40b1a5f55ce8747f3e8042641e7f6258d625ab7
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-07-26 17:18:25 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,9 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot (*talk = "Squawk!")
+def parrot (talk = "Squawk!")
   puts "#{talk}"
   return "#{talk}"
-  
-  puts "Squawk!"
-  return "Squawk!"
 end
